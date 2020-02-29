996bc2209f5c0d1cc4d6d56acf9ec3e8658628de
Seal125 <stephaniesmith12514@gmail.com>
2020-02-29 18:42:44 +0000

    Adds heatmap layer, example data only. Has invalid value error

--- src/app.js
+++ src/app.js
@@ -1,9 +1,37 @@
+let map;
+let heatmap;
+
+function locationData() {
+  return [
+    { location: new google.maps.LatLng(37.782, -122.447), weight: 4 },
+    { location: new google.maps.LatLng(37.782, -122.445), weight: 5 },
+    { location: new google.maps.LatLng(37.782, -122.443), weight: 3 },
+    { location: new google.maps.LatLng(37.782, -122.441), weight: 6 },
+    { location: new google.maps.LatLng(37.782, -122.439), weight: 4 },
+    { location: new google.maps.LatLng(37.782, -122.437), weight: 2 },
+    { location: new google.maps.LatLng(37.782, -122.435), weight: 8 },
+    { location: new google.maps.LatLng(37.785, -122.447), weight: 3 },
+    { location: new google.maps.LatLng(37.785, -122.445), weight: 9 },
+    { location: new google.maps.LatLng(37.785, -122.443), weight: 5 },
+    new google.maps.LatLng(37.785, -122.441),
+    new google.maps.LatLng(37.785, -122.439),
+    new google.maps.LatLng(37.785, -122.437),
+    new google.maps.LatLng(37.785, -122.435)
+  ]
+}
+
 function initMap() {
-  // The location of Uluru
-  var uluru = { lat: -25.344, lng: 131.036 };
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-    document.getElementById('map'), { zoom: 4, center: uluru });
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({ position: uluru, map: map });
+  let sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
+  map = new google.maps.Map(
+    document.getElementById('map'), {
+      zoom: 11,
+      center: sanFrancisco,
+      mapTypeId: 'satellite'
+    });
+
+  heatmap = new google.maps.visualization.HeatmapLayer({
+    data: locationData,
+    map: map
+  });
+  heatmap.setMap(map)
 }
